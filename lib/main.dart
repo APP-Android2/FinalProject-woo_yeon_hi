b93da5fcb9f3ad944a2085c8fa52af2a43e6c0cc
oscar <oscarrr0225@gmail.com>
2024-05-29 17:20:39 +0900

    :sparkles: Add 카카오 로그인 기능 추가
    Related to:#5

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:woo_yeon_hi/provider/tab_page_index_provider.dart';
+import 'package:woo_yeon_hi/provider/user_control_provider.dart';
 import 'package:woo_yeon_hi/routes/routes_generator.dart';
 import 'package:woo_yeon_hi/screen/register/register_screen.dart';
 import 'package:woo_yeon_hi/style/color.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:woo_yeon_hi/widget/login/kakao_login.dart';
+import 'package:woo_yeon_hi/widget/register/google_sign_in.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
 
-  initializeDateFormatting().then((_) => runApp(WooYeonHi()));
+  KakaoSdk.init(
+    nativeAppKey: '62a1ea96953fba93f6d4e5b355e2d455',
+    javaScriptAppKey: '71b46fb5f5a68ad3d8aaa257ffa704fa',
+  );
 
   runApp(const WooYeonHi());
-}
 
+}
 class WooYeonHi extends StatefulWidget {
   const WooYeonHi({super.key});
 
   @override
   State<WooYeonHi> createState() => _WooYeonHiState();
 }
+
 
 class _WooYeonHiState extends State<WooYeonHi> {
   @override
@@ -32,13 +41,8 @@ class _WooYeonHiState extends State<WooYeonHi> {
           ),
           useMaterial3: true
       ),
-      home: const DefaultTabController(
-        initialIndex: 2,
-        length: 5,
-        child: Scaffold(
-          body: RegisterScreen(),
-        ),
-      ),
+      home: RegisterScreen(),
+
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
